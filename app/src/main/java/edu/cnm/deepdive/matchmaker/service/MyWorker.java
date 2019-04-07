06b99d06a242bc223c85e1baa8cb0f8467d9e535
MichaelGSanchez <michaelgregorysanchez@gmail.com>
2019-04-07 13:44:50 -0600

    Login screen styled.  Working on styling fragment_find_someone

---
+++ app/src/main/java/edu/cnm/deepdive/matchmaker/service/MyWorker.java
@@ -0,0 +1,24 @@
+package edu.cnm.deepdive.matchmaker.service;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.media.MediaBrowserServiceCompat.Result;
+import android.util.Log;
+import com.facebook.stetho.inspector.protocol.module.Worker;
+
+public class MyWorker extends Worker {
+
+  private static final String TAG = "MyWorker";
+
+/*  public MyWorker(@NonNull Context appContext, @NonNull WorkerParameters workerParams) {
+    super(appContext, workerParams);
+  }
+
+  @NonNull
+  @Override
+  public Result doWork() {
+    Log.d(TAG, "Performing long running task in scheduled job");
+    // TODO(developer): add long running task here.
+    return Result.success();
+  }*/
+}
