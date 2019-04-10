261f28f0706daef70ddff48ddc5787b536492ea7
MichaelGSanchez <michaelgregorysanchez@gmail.com>
2019-04-10 10:55:10 -0600

    Added java docs to file, needs to still generate, having errors show at the moment

--- app/src/main/java/edu/cnm/deepdive/matchmaker/model/entity/dao/userDao.java
+++ app/src/main/java/edu/cnm/deepdive/matchmaker/model/entity/dao/userDao.java
@@ -1,5 +1,17 @@
 package edu.cnm.deepdive.matchmaker.model.entity.dao;
 
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import edu.cnm.deepdive.matchmaker.model.entity.User;
+import java.util.List;
+
 public interface userDao {
+
+  @Insert
+  List<String> insert(User users);
+
+  @Query
+      ("SELECT * FROM User")
+  List<User> findAll();
 
 }
--- app/src/main/java/edu/cnm/deepdive/matchmaker/model/entity/dao/userDecisionDao.java
+++ app/src/main/java/edu/cnm/deepdive/matchmaker/model/entity/dao/userDecisionDao.java
@@ -1,5 +1,15 @@
 package edu.cnm.deepdive.matchmaker.model.entity.dao;
 
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import edu.cnm.deepdive.matchmaker.model.entity.User;
+
 public interface userDecisionDao {
 
+  @Insert(onConflict = OnConflictStrategy.IGNORE)
+  Boolean decision(User user);
+
+  @Query("SELECT * FROM User ")
+  Boolean decision ();
 }
